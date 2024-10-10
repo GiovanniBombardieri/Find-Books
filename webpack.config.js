7f9a61998ca38ab41ac88e3ec96e5c73d546c778
GiovanniBombardieri <g.bombardieri06@gmail.com>
2024-10-10 14:40:15 +0200

    fixed problems with tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,28 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
+    clean: true,
     filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
       },
     ],
   },
