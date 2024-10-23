282b794069d3515b03e37449172fabd4562d3b7d
GiovanniBombardieri <g.bombardieri06@gmail.com>
2024-10-23 15:20:38 +0200

    some change for testing with pre-written table of elements

--- src/js/index.js
+++ src/js/index.js
@@ -4,59 +4,95 @@ import "../css/style.css";
 // Variables
 const btn = document.querySelector("#btn");
 const genre = document.querySelector("#genre-input");
-const table = document.querySelector("#table");
 const bodyTable = document.querySelector("#body-table");
+const tableContainer = document.querySelector("#container");
 
 const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("dialog button");
 
-// "Show the dialog" button opens the dialog modally
-showButton.addEventListener("click", () => {
+// Read description of single book
+function readDescription(description) {
+  const dialogText = document.querySelector("#dialog-text");
+  dialogText.innerHTML = description;
   dialog.showModal();
-});
-
+}
 // "Close" button closes the dialog
 closeButton.addEventListener("click", () => {
   dialog.close();
 });
 
-// Search event by user
 btn.addEventListener("click", () => {
-  if (bodyTable.innerHTML === "") {
-    getTitle();
-  } else {
-    bodyTable.innerHTML = "";
-    getTitle();
-  }
-});
-genre.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    if (bodyTable.innerHTML === "") {
-      e.preventDefault();
-      getTitle();
-    } else {
-      bodyTable.innerHTML = "";
-      e.preventDefault();
-      getTitle();
-    }
-  }
-});
+  genre.value = "";
+  tableContainer.classList.remove("hidden");
+  const titleId1 = document.querySelector("#title-id1");
+  const titleId2 = document.querySelector("#title-id2");
+  const titleId3 = document.querySelector("#title-id3");
+  const titleId4 = document.querySelector("#title-id4");
+  const titleId5 = document.querySelector("#title-id5");
+  const descriptionTitle1 = document.querySelector("#description-title-1");
+  const descriptionTitle2 = document.querySelector("#description-title-2");
+  const descriptionTitle3 = document.querySelector("#description-title-3");
+  const descriptionTitle4 = document.querySelector("#description-title-4");
+  const descriptionTitle5 = document.querySelector("#description-title-5");
 
-function readDescription() {
-  const titleId = document.querySelector("#titleId");
-  titleId.addEventListener("click", () => {
-    dialog.showModal();
+  let description1 = descriptionTitle1.innerHTML;
+  let description2 = descriptionTitle2.innerHTML;
+  let description3 = descriptionTitle3.innerHTML;
+  let description4 = descriptionTitle4.innerHTML;
+  let description5 = descriptionTitle5.innerHTML;
+
+  titleId1.addEventListener("click", () => {
+    readDescription(description1);
   });
-}
+  titleId2.addEventListener("click", () => {
+    readDescription(description2);
+  });
+  titleId3.addEventListener("click", () => {
+    readDescription(description3);
+  });
+  titleId4.addEventListener("click", () => {
+    readDescription(description4);
+  });
+  titleId5.addEventListener("click", () => {
+    readDescription(description5);
+  });
+});
 
 // Event to reset
 genre.addEventListener("focus", () => {
-  bodyTable.innerHTML = "";
-  table.classList.add("hidden");
+  console.log("Hello");
+  tableContainer.classList.add("hidden");
 });
 
-// Function for display title
+// Search event by user
+// btn.addEventListener("click", () => {
+//   if (bodyTable.innerHTML === "") {
+//     getTitle();
+//   } else {
+//     bodyTable.innerHTML = "";
+//     getTitle();
+//   }
+// });
+// genre.addEventListener("keypress", function (e) {
+//   if (e.key === "Enter") {
+//     if (bodyTable.innerHTML === "") {
+//       e.preventDefault();
+//       getTitle();
+//     } else {
+//       bodyTable.innerHTML = "";
+//       e.preventDefault();
+//       getTitle();
+//     }
+//   }
+// });
+
+// Event to reset
+// genre.addEventListener("focus", () => {
+//   bodyTable.innerHTML = "";
+//   table.classList.add("hidden");
+// });
+
+// Function for display author and title
 function getTitle() {
   if (genre.value === "") {
     alert("Write a valid genre, please.");
